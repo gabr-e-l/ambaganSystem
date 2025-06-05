bd980d9b1570e7dabecf66dc32df0de9ab3824b4
gabr-e-l <aiellogabriellastrella@gmail.com>
2025-06-06 00:06:05 +0630

    update

--- DataLayer/DBDataService.cs
+++ DataLayer/DBDataService.cs
@@ -1,37 +1,4 @@
-﻿/*using AmbagCommon;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataLayer
-{
-    public class DBDataService : IAmbagDataService
-    {
-        public void AddEntry(AmbagData.AmbagEntry entry)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<AmbagData.AmbagEntry> GetAllEntries()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool RemoveEntry(string listName, string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool RemoveList(string listName)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}*/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 using static AmbagCommon.AmbagData;
